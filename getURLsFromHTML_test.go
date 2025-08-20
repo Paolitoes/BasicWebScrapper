5cb33b27e7dee5fe67296172557da9777ee875ac
Paolitoes <carlospaolo15@gmail.com>
2025-08-20 13:05:12 -0400

    expanded unit test 1

--- getURLsFromHTML_test.go
+++ getURLsFromHTML_test.go
@@ -12,14 +12,28 @@ func TestGetURLsFromHTML(t *testing.T) {
 		inputURL	string
 		inputBody	string
 		expected	[]string
+		containsError	string
 	}{
 		{
-			name: 		"absolute and relative URLs",
+			name: 		"relative URL",
 			inputURL: 	"https://blog.boot.dev",
 			inputBody: `
 				<html>
 					<body>
 						<a href="/path/one">
+							<span>Boot.dev</span>
+						</a>
+					</body>
+				</html>
+				`,
+			expected: 	[]string{"https://other.com/path/one"},
+		},{
+			name: 		"absolute URLs",
+			inputURL: 	"https://blog.boot.dev",
+			inputBody: `
+				<html>
+					<body>
+						<a href="https://blog.boot.dev/path/one">
 							<span>Boot.dev</span>
 						</a>
 						<a href="https://other.com/path/one">
@@ -30,12 +44,12 @@ func TestGetURLsFromHTML(t *testing.T) {
 				`,
 			expected: 	[]string{"https://blog.boot.dev/path/one","https://other.com/path/one"},
 		},{
-			name: 		"absolute URLs",
+			name: 		"absolute and relative URLs",
 			inputURL: 	"https://blog.boot.dev",
 			inputBody: `
 				<html>
 					<body>
-						<a href="https://blog.boot.dev/path/one">
+						<a href="/path/one">
 							<span>Boot.dev</span>
 						</a>
 						<a href="https://other.com/path/one">
